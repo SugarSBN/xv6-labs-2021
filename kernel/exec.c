7d5c5f6350e8fa74eb973de2e6f86f081c3d235f
SugarSBN <15952265117@126.com>
2022-03-20 13:07:39 +0800

    pgtbl-solutions-PrintPagetable

--- kernel/exec.c
+++ kernel/exec.c
@@ -8,6 +8,7 @@
 #include "elf.h"
 
 static int loadseg(pde_t *pgdir, uint64 addr, struct inode *ip, uint offset, uint sz);
+void vmprint(pagetable_t pagetable, uint64 depth);
 
 int
 exec(char *path, char **argv)
@@ -115,6 +116,8 @@ exec(char *path, char **argv)
   p->trapframe->epc = elf.entry;  // initial program counter = main
   p->trapframe->sp = sp; // initial stack pointer
   proc_freepagetable(oldpagetable, oldsz);
+
+  if (p->pid == 1)  vmprint(p->pagetable, 0);
 
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,20 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void 
+vmprint(pagetable_t pagetable, uint64 depth){
+  if (depth == 0) printf("page table %p\n", pagetable);
+  if (depth > 2)  return;
+
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      for (int j = 0;j < depth;j++) printf(".. ");
+      printf("..%d: pte %p pa %p\n", i, pte, child);
+      vmprint((pagetable_t)child, depth + 1);
+    }
+  }
+}
